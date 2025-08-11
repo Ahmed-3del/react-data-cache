6f010bf31d860fb08efcc049745a05d4c0f111ba
Ahmed adel <itsahmedad@gmail.com>
2025-08-11 22:32:56 +0300

    feat: publish react-data-cache

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { useData } from "./useData";
 export { prefetchData, prefetchMulti, prefetchOnEvent } from "./prefetch";
 export { clearDataCache } from "./cache";
 export * from "./types";
+export * from "./cache";
+export * from "./prefetch";
+export * from "./useData";
+export * from "./useInfiniteQuery";
+export * from "./useScrollToLoad";
